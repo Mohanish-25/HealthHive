1e4811f1aef8b3a8f618b65c03cb3164b6e65a15
Mohanish-25 <mohanisdesale@gmail.com>
2025-11-13 20:43:43 +0530

    Update StatusBar for consistent styling

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
+import { StatusBar} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator'; // centralized navigator (uses AuthStack internally)
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
 
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
     </SafeAreaProvider>
   );
 }
